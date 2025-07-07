233ca44caad9d96612c509e61e43577cde3aa66e
corn-snake <cornsnenk@gmail.com>
2025-07-07 06:35:14 +0100

    initial student's datafetching. will separate admin displayClases-renderClases from teach/student

--- routing/alumnos.js
+++ routing/alumnos.js
@@ -1,7 +1,10 @@
 import { Router } from "@oak/oak";
 import { HTMLPath, render } from "../lib/compose.js";
+import { getPupilFront } from "../lib/sql.js";
+import { decodeToken } from "../lib/jwt.js";
 
 const alumnos = new Router()
-    .get("/", async ctx => ctx.response.body = render(await HTMLPath("alumnos/head.html"), await HTMLPath("loggedInTopbar.html"), await HTMLPath("tail.html")));
+    .get("/", async ctx => ctx.response.body = render(await HTMLPath("alumnos/head.html"), await HTMLPath("loggedInTopbar.html"), await HTMLPath("tail.html")))
+    .post("/", async ctx => ctx.response.body = await getPupilFront(await decodeToken(await ctx.request.body.text())));
 
 export default alumnos;
